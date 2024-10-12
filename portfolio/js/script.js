8c6e5cb02d40fec618894ce56f27c9f8402dfcc2
yashjain2711 <123976840+yashjain2711@users.noreply.github.com>
2024-10-13 04:56:10 +0530

    CodeAlpha Task3
    Portfolio website

---
+++ portfolio/js/script.js
@@ -0,0 +1,41 @@
+	// Sticky Navbar
+    let header = document.querySelector('header');
+    let menu = document.querySelector('#menu-icon');
+    let navbar = document.querySelector('.navbar');
+     
+     
+    window.addEventListener('scroll', () => {
+        header.classList.toggle('shadow', window.scrollY > 0);
+    });
+     
+    menu.onclick = () => {
+        navbar.classList.toggle('active');
+    }
+    window.onscroll = () => {
+        navbar.classList.remove('active');
+    }
+     
+    // Dark Mode
+    let darkmode = document.querySelector('#darkmode');
+     
+    darkmode.onclick = () => {
+        if(darkmode.classList.contains('bx-moon')){
+            darkmode.classList.replace('bx-moon','bx-sun');
+            document.body.classList.add('active');
+        }else{
+            darkmode.classList.replace('bx-sun','bx-moon');
+            document.body.classList.remove('active');
+        }
+    }
+
+    
+    gsap.from("h1",{
+            y:50,
+            duration:2,
+            delay:1,
+            backgroundColor:'red',
+            rotate:360,
+            borderRadius:'50%',
+            scale:0.5
+            stagger:1
+        })
